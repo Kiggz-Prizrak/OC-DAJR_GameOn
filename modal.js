bf2363fc9f4199f25cbfec896a39df5a9a4c5ccc
Kiggz <nico.peignot@outlook.fr>
2022-09-18 15:49:02 +0200

    Modal closer function & dom Element list

--- modal.js
+++ modal.js
@@ -11,13 +11,21 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalCloser = document.querySelector(
+  "body > main > div.bground > div > span"
+);
+
+const checkNextEvent = document.getElementById("checkbox2").checked;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
-
+// Close modal event
+modalCloser.addEventListener("click", closeModal);
+// close modal formData
+function closeModal() {
+  modalbg.style.display = "none";
+}
